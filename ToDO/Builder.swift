106da02e2c3889506e674bea19098ca13e303e91
europeec <l.europeec@ya.ru>
2021-10-06 14:33:29 +0300

    create builder + router

---
+++ ToDO/Builder.swift
@@ -0,0 +1,26 @@
+//
+//  Builder.swift
+//  ToDO
+//
+//  Created by Дмитрий Юдин on 06.10.2021.
+//
+
+import UIKit
+
+protocol BuilderProtocol: AnyObject {
+    func createMainModule(router: RouterProtocol) -> UIViewController
+    func createAddModule(router: RouterProtocol) -> UIViewController
+}
+
+class Builder: BuilderProtocol {
+    func createMainModule(router: RouterProtocol) -> UIViewController {
+        let view = MainViewController()
+        let presenter = MainPresenter(view: view, router: router)
+        view.presenter = presenter
+        return view
+    }
+    
+    func createAddModule(router: RouterProtocol) -> UIViewController {
+        //
+    }
+}
--- ToDO/Router.swift
+++ ToDO/Router.swift
@@ -7,6 +7,43 @@
 
 import UIKit
 
-protocol RouterProtocol: AnyObject {
+protocol RouterMainProtocol: AnyObject {
+    var navigationController: UINavigationController? { get set }
+    var builder: BuilderProtocol? { get set }
+    func initialMainViewController()
+}
+
+protocol RouterProtocol: RouterMainProtocol {
+    func presentAddScreen()
+    func popToRoot()
+}
+
+class Router: RouterProtocol {
+    var navigationController: UINavigationController?
+    var builder: BuilderProtocol?
     
+    init(navigationController: UINavigationController?, builder: BuilderProtocol?) {
+        self.navigationController = navigationController
+        self.builder = builder
+    }
+    
+    func initialMainViewController() {
+        if let navigationController = navigationController {
+            guard let mainViewController = builder?.createMainModule(router: self) else { return }
+            navigationController.viewControllers = [mainViewController]
+        }
+    }
+    
+    func presentAddScreen() {
+        if let navigationController = navigationController {
+            guard let addViewController = builder?.createAddModule(router: self) else { return }
+            navigationController.viewControllers = [addViewController]
+        }
+    }
+    
+    func popToRoot() {
+        if let navigationController = navigationController {
+            navigationController.popToRootViewController(animated: true)
+        }
+    }
 }
